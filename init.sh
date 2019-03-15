425cc5071ee825087e55a62067bf7dcc5100d761
谭文海 <tanwenhai@outlook.com>
2019-03-15 16:27:11 +0800

    docker
    docker

--- init.sh
+++ init.sh
@@ -1,5 +1,37 @@
 echo 'install zsh'
 
 # Oh My Zsh
+echo '#### begin install zsh ####'
 yum -y install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo '#### end install zsh ####'
+
+# Docker
+echo '#### begin install docker ####'
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+    
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
+
+
+echo '#### end install docker ####'
