17728824e168c9ff3d3d79489574db934afd03ee
谭文海 <tanwenhai@outlook.com>
2019-04-13 10:07:09 +0800

    docker-composer

--- init.sh
+++ init.sh
@@ -37,5 +37,7 @@ sudo systemctl start docker
 
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo '#### end install docker ####'
